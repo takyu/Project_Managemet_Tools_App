b0fc783b77579b263d634f01d29ead2d402cc1eb
takyu <takyu613.study@gmail.com>
2022-04-01 03:10:16 +0900

    manipulate DOM Element

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,68 @@
-const app = document.querySelector('#app')! as HTMLDivElement;
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
-app.insertAdjacentHTML('afterbegin', '<h1>Hello, World!</h1>');
+  /**
+   * コンストラクタ関数では、基本的に要素への参照を定義するのに使う
+   * それ以外は、各メソッドに分ける
+   */
+  constructor() {
+    /**
+     * <template>
+     * #document-fragment
+     * <form>
+     */
+    this.templateElement = document.querySelector(
+      '#project-input'
+    )! as HTMLTemplateElement;
+
+    this.hostElement = document.querySelector('#app')! as HTMLDivElement;
+
+    // this.templateElement に属するノードをクローンする
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    // #document-fragment の最初のひとつ目の form を取り出す
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+
+    // id を付与しスタイルをつける
+    this.element.id = 'user-input';
+
+    // form のそれぞれの input の参照
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector(
+      '#manday'
+    ) as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  private submitHandler(e: Event) {
+    // HTTP request が送られないようにする。
+    e.preventDefault();
+
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
