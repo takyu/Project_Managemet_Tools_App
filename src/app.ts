91837c5cb9023409e384f9bad3fbf64e2266dab9
takyu <takyu613.study@gmail.com>
2022-04-01 11:49:50 +0900

    create validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,61 @@
+// Validation
+type Validatable = {
+  value: string | number;
+  required?: boolean; // required: boolean | undefined と同義
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+};
+
+function validate(validatableInput: Validatable): boolean {
+  let isValid = true;
+
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    /**
+     * if (validatavleInput) としてしまうと、minLength = 0 を設定している場合、
+     * falsy 判定になり、条件に反するということになる。
+     *
+     * そこで、minLength != null とする事で、null と undefined 以外は条件を通る
+     * という書き方をするのがベター
+     * （ minLength !== undefined とすると、undefined 以外という判定になる
+     * 安全性を高めるといった観点では、 minLength != null の方が良い）
+     *
+     * また、そもそも minLength = 0 は、required のチェックとほぼ同じなので、
+     * チェックする必要があるのかという議論の余地がある。。
+     */
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+
+  return isValid;
+}
+
 // autobind decorator
 function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
@@ -63,14 +121,33 @@ class ProjectInput {
   }
 
   private gatherUserInput(): [string, string, number] | void {
+    // 各フォームの値
     const enteredTitle = this.titleInputElement.value;
     const enteredDescription = this.descriptionInputElement.value;
     const enteredManday = this.mandayInputElement.value;
 
+    // 各フォームの値とチェックの設定を合わせたオブジェクト
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 10,
+    };
+    const MandayValidatable: Validatable = {
+      value: +enteredManday,
+      required: true,
+      min: 1,
+      max: 100,
+    };
+
+    // バリデーション
     if (
-      enteredTitle.trim().length === 0 ||
-      enteredDescription.trim().length === 0 ||
-      enteredManday.trim().length === 0
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(MandayValidatable)
     ) {
       alert('入力値が正しくありません。再度入力してください。');
       return;
